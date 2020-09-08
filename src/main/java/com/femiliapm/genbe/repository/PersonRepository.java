6bfead37d8a67ce63983a0cb58168a964e04ac02
Femilia Putri Mayranti <femiliaputrimayranti@gmail.com>
2020-09-08 07:15:36 +0700

    Updated person repository

--- src/main/java/com/femiliapm/genbe/repository/PersonRepository.java
+++ src/main/java/com/femiliapm/genbe/repository/PersonRepository.java
@@ -1,7 +1,5 @@
 package com.femiliapm.genbe.repository;
 
-//import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,6 +8,5 @@ import com.femiliapm.genbe.model.entity.PersonEntity;
 
 @Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, Integer>{
-//	@Query(value = "SELECT * FROM t_biodata AS tb INNER JOIN t_person AS tp ON tb.idperson = tp.id_person")
-//		List<PersonEntity> fullBiodata();
+//	public PersonEntity findByNik(String nik);
 }
