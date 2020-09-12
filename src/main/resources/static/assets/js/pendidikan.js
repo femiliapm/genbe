2ab61828b2eb01a57e3514fa4b2f96f96c73d6bb
Femilia Putri Mayranti <femiliaputrimayranti@gmail.com>
2020-09-12 11:11:58 +0700

    Add alert to website

--- src/main/resources/static/assets/js/pendidikan.js
+++ src/main/resources/static/assets/js/pendidikan.js
@@ -1,5 +1,5 @@
 var tableBiodata = {
-    create: function () {
+    create: function() {
         // jika table tersebut datatable, maka clear and dostroy
         if ($.fn.DataTable.isDataTable('#tableBiodata')) {
             //table yg sudah dibentuk menjadi datatable harus d rebuild lagi untuk di instantiasi ulang
@@ -10,49 +10,48 @@ var tableBiodata = {
         $('#tableBiodata').DataTable({
             data: input,
             columns: [{
-                    title: "Jenjang",
-                    data: "jenjang"
-                }, {
-                    title: "Institusi",
-                    data: "institusi"
-                }, {
-                    title: "Tahun Masuk",
-                    data: "masuk"
-                }, {
-                    title: "Tahun Lulus",
-                    data: "lulus"
-                }, {
-                    title: "Action",
-                    data: null,
-                    render: function (data, type, row, meta) {
-                        return "<button class='btn-primary' onclick=formBiodata.setEditData('" + meta.row + "')>Edit</button>"
-                    }
+                title: "Jenjang",
+                data: "jenjang"
+            }, {
+                title: "Institusi",
+                data: "institusi"
+            }, {
+                title: "Tahun Masuk",
+                data: "masuk"
+            }, {
+                title: "Tahun Lulus",
+                data: "lulus"
+            }, {
+                title: "Action",
+                data: null,
+                render: function(data, type, row, meta) {
+                    return "<button class='btn-primary' onclick=formBiodata.setEditData('" + meta.row + "')>Edit</button>"
                 }
-            ]
+            }]
         });
     }
 };
 
 var formBiodata = {
-    resetForm: function () {
+    resetForm: function() {
         $('#form-biodata')[0].reset();
     },
 
-    saveFormPend: function () {
+    saveFormPend: function() {
         var dataResult = getJsonForm($('#form-biodata').serializeArray(), true);
         input.push(dataResult);
         tableBiodata.create();
         $('#modal-biodata').modal('hide')
     },
 
-    submitFormPend: function () {
+    submitFormPend: function() {
         $.ajax({
             url: '/api/pendidikan/' + $('#idPerson').val(),
             method: 'post',
             contentType: 'application/json',
             dataType: 'json',
             data: JSON.stringify(input),
-            success: function (res, status, xhr) {
+            success: function(res, status, xhr) {
                 $('#idPerson').val('');
                 input = [];
                 if ($.fn.DataTable.isDataTable('#tableBiodata')) {
@@ -60,20 +59,21 @@ var formBiodata = {
                     $('#tableBiodata').DataTable().clear();
                     $('#tableBiodata').DataTable().destroy();
                 };
+                alert('{status: "true", message: "data berhasil masuk"}');
             },
-            error: function (err) {
+            error: function(err) {
                 console.log(err);
             }
         });
     },
 
-    setEditData: function (index) {
+    setEditData: function(index) {
         $('#form-biodata').fromJSON(JSON.stringify(input[index]));
         $('#modal-biodata').modal('show')
         newIn = index
     },
 
-    saveFormPendEdit: function () {
+    saveFormPendEdit: function() {
         var dataResult = getJsonForm($('#form-biodata').serializeArray(), true);
         input[newIn] = dataResult;
         tableBiodata.create();
--- src/main/resources/static/assets/js/person.js
+++ src/main/resources/static/assets/js/person.js
@@ -1,5 +1,5 @@
 var tableBiodata = {
-    create: function () {
+    create: function() {
         // jika table tersebut datatable, maka clear and destroy
         if ($.fn.DataTable.isDataTable('#tableBiodata')) {
             //table yg sudah dibentuk menjadi datatable harus d rebuild lagi untuk di instantiasi ulang
@@ -11,42 +11,41 @@ var tableBiodata = {
             url: '/person',
             type: 'get',
             contentType: 'application/json',
-            success: function (res, status, xhr) {
+            success: function(res, status, xhr) {
                 if (xhr.status == 200 || xhr.status == 201) {
                     $('#tableBiodata').DataTable({
                         data: res,
                         columns: [{
-                                title: "NIK",
-                                data: "nik"
-                            }, {
-                                title: "Nama Lengkap",
-                                data: "name"
-                            }, {
-                                title: "Alamat",
-                                data: "address"
-                            }, {
-                                title: "No. Hp",
-                                data: "hp"
-                            }, {
-                                title: "Tanggal Lahir",
-                                data: "tgl"
-                            }, {
-                                title: "Tempat Lahir",
-                                data: "tempatLahir"
-                            }, {
-                                title: "Action",
-                                data: null,
-                                render: function (data, type, row) {
-                                    console.log(data)
-                                    return "<button class='btn-primary' onclick=formBiodata.setEditData('" + data.idBio + "')>Edit</button>"
-                                }
+                            title: "NIK",
+                            data: "nik"
+                        }, {
+                            title: "Nama Lengkap",
+                            data: "name"
+                        }, {
+                            title: "Alamat",
+                            data: "address"
+                        }, {
+                            title: "No. Hp",
+                            data: "hp"
+                        }, {
+                            title: "Tanggal Lahir",
+                            data: "tgl"
+                        }, {
+                            title: "Tempat Lahir",
+                            data: "tempatLahir"
+                        }, {
+                            title: "Action",
+                            data: null,
+                            render: function(data, type, row) {
+                                console.log(data)
+                                return "<button class='btn-primary' onclick=formBiodata.setEditData('" + data.idBio + "')>Edit</button>"
                             }
-                        ]
+                        }]
                     });
 
                 } else {}
             },
-            error: function (err) {
+            error: function(err) {
                 console.log(err);
             }
         });
@@ -55,10 +54,10 @@ var tableBiodata = {
 };
 
 var formBiodata = {
-    resetForm: function () {
+    resetForm: function() {
         $('#form-biodata')[0].reset();
     },
-    saveForm: function () {
+    saveForm: function() {
         if ($('#form-biodata').parsley().validate()) {
             var dataResult = getJsonForm($("#form-biodata").serializeArray(), true);
 
@@ -68,7 +67,7 @@ var formBiodata = {
                 contentType: 'application/json',
                 dataType: 'json',
                 data: JSON.stringify(dataResult),
-                success: function (res, status, xhr) {
+                success: function(res, status, xhr) {
                     var response = res.status;
                     var message = res.message;
                     console.log(response);
@@ -83,14 +82,14 @@ var formBiodata = {
                         alert('{status: "false", message: "data gagal masuk, umur kurang dari 30 tahun"}');
                     } else {}
                 },
-                error: function (err) {
+                error: function(err) {
                     console.log(err);
                     console.log(JSON.stringify(dataResult))
                 }
             });
         }
     },
-    setEditData: function (idCabang) {
+    setEditData: function(idCabang) {
         formBiodata.resetForm();
 
         $.ajax({
@@ -98,19 +97,19 @@ var formBiodata = {
             method: 'get',
             contentType: 'application/json',
             dataType: 'json',
-            success: function (res, status, xhr) {
+            success: function(res, status, xhr) {
                 if (xhr.status == 200 || xhr.status == 201) {
                     $('#form-biodata').fromJSON(JSON.stringify(res));
                     $('#modal-biodata').modal('show')
 
                 } else {}
             },
-            error: function (err) {
+            error: function(err) {
                 console.log(err);
             }
         });
     },
-    getDataByNik: function (nik) {
+    getDataByNik: function(nik) {
         if ($.fn.DataTable.isDataTable('#tableBiodata')) {
             //table yg sudah dibentuk menjadi datatable harus d rebuild lagi untuk di instantiasi ulang
             $('#tableBiodata').DataTable().clear();
@@ -122,52 +121,51 @@ var formBiodata = {
             method: 'get',
             contentType: 'application/json',
             dataType: 'json',
-            success: function (result) {
+            success: function(result) {
                 if (result[0].status == 'true' && result[0].message == 'success') {
                     $('#tableBiodata').DataTable({
                         data: [result[0].data],
                         columns: [{
-                                title: "NIK",
-                                data: "nik"
-                            }, {
-                                title: "Nama Lengkap",
-                                data: "name"
-                            }, {
-                                title: "Alamat",
-                                data: "address"
-                            }, {
-                                title: "No. Hp",
-                                data: "hp"
-                            }, {
-                                title: "Tanggal Lahir",
-                                data: "tgl"
-                            }, {
-                                title: "Tempat Lahir",
-                                data: "tempatLahir"
-                            }, {
-                                title: "Umur",
-                                data: "umur"
-                            }, {
-                                title: "Pendidikan Terakhir",
-                                data: "pendidikan_terakhir"
-                            }, {
-                                title: "Action",
-                                data: null,
-                                render: function (data, type, row) {
-                                    return "<button class='btn-primary' onclick=formBiodata.setEditData('" + data.id + "')>Edit</button>"
-                                }
+                            title: "NIK",
+                            data: "nik"
+                        }, {
+                            title: "Nama Lengkap",
+                            data: "name"
+                        }, {
+                            title: "Alamat",
+                            data: "address"
+                        }, {
+                            title: "No. Hp",
+                            data: "hp"
+                        }, {
+                            title: "Tanggal Lahir",
+                            data: "tgl"
+                        }, {
+                            title: "Tempat Lahir",
+                            data: "tempatLahir"
+                        }, {
+                            title: "Umur",
+                            data: "umur"
+                        }, {
+                            title: "Pendidikan Terakhir",
+                            data: "pendidikan_terakhir"
+                        }, {
+                            title: "Action",
+                            data: null,
+                            render: function(data, type, row) {
+                                return "<button class='btn-primary' onclick=formBiodata.setEditData('" + data.id + "')>Edit</button>"
                             }
-                        ]
+                        }]
                     });
                     alert('{status: "true", message: "success"}');
                 } else if (result[0].status == 'true' && result[0].message.includes('ditemukan')) {
                     alert('{status: "true", message: "data dengan nik ' + nik + ' tidak tersedia"}');
                 } else if (result[0].status == 'false' && result[0].message.includes('gagal')) {
-                    alert('{status: "true", message: "data dengan nik ' + nik + ' tidak tersedia"}');					
-				}
+                    alert('{status: "false", message: "data gagal masuk, jumlah digit nik tidak sama dengan 16"');
+                }
                 console.log(result);
             },
-            error: function (err) {
+            error: function(err) {
                 console.log(err);
             }
         });
